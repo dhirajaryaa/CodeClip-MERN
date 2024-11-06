94af9f30819ab4c493d42ff7927ce3bcc4b9a6c0
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-11-06 07:25:13 +0530

    feat: generator for jwt access token and refresh token for security Reason

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema(
   {
@@ -64,6 +65,32 @@ userSchema.method.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(this.password, password);
 };
 
+// custom methods for jwt token
+userSchema.method.isAccessTokenGenerator = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      emailId: this.emailId,
+      fullName: this.fullName,
+    },
 
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.method.isRefreshTokenGenerator = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model('User', userSchema);
